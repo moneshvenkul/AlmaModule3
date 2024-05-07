697d60311728c2936d6338ae71862b3e0948ecde
Monesh Venkul Vommi <mvvenkul@gmail.com>
2024-05-07 12:19:29 -0400

    Delete react component example/src directory

--- react component example/src/Details.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-class Details extends Component{
-    render(){
-       return <p> class: {this.props.class} marks: {this.props.marks}</p>
-    }
-}
-
-export default Details;
--- react component example/src/welcome.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-class Welcome extends Component{
-    render(){
-        return <h1> Hello, {this.props.name} {this.props.age} </h1>;
-    }
-}
-
-export default Welcome;
